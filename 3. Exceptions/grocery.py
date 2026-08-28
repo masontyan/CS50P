3581bf27a81f39bf5dd63bcde85948563f2f991e
Mason Yan <masonyan@Masons-MacBook-Pro.local>
2026-08-27 23:32:24 -0700

    Complete grocery problem

--- 3. Exceptions/grocery.py
+++ 3. Exceptions/grocery.py
@@ -1,19 +1,40 @@
 def main():
-
     groceries = {}
 
     while True:
         try:
-            item = input()
+            item = input().upper()
         except EOFError:
             break
 
         if item in groceries:
-            groceries[item] =+ 1
+            groceries[item] += 1
         else:
             groceries[item] = 1
 
-    for item in groceries:
-        print(groceries[item])
+    for item in sorted(groceries):
+        print(groceries[item], (item))
+
 
 main()
+
+
+# def main():
+#     groceries = {}
+
+#     while True:
+#         try:
+#             item = input().upper()
+#         except EOFError:
+#             break
+
+#         if item in groceries:
+#             groceries[item] += 1
+#         else:
+#             groceries[item] = 1
+
+#     for item in sorted(groceries):
+#         print(groceries[item], item)
+
+
+# main()
