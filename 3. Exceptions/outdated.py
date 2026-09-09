929d0b12b73ce774b6d8455ea767b5dba2db7148
Mason Yan <masonyan@Masons-MacBook-Pro.local>
2026-09-09 05:47:37 -0700

    wip: outdated problem

---
+++ 3. Exceptions/outdated.py
@@ -0,0 +1,62 @@
+# prompt user for date in month day year order
+
+# then output the saem date in yyyy mm dd format
+
+# if users input is not a valid date in either format, primpt the user again
+
+# assume all months has <31 days
+
+def main():
+
+    while True:
+        try:
+            date = input("Date: ")
+
+            months = [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+                        ]
+
+            if "/" in date:
+                month, day, year = date.split("/") #but you also need to know how to get the other format of valid input...
+
+                month = int(month)
+                day = int(day)
+                year = int(year)
+
+            elif "," in date:
+
+                month, day, year = date.split()
+
+                month = int(months.index(month) +1)
+                day = int(day.strip(","))
+                year = int(year)
+
+            elif "/" or "," not in date:
+                continue
+
+            if not 1 <= month <= 12:
+                continue
+            if not 1 <= day <= 31:
+                continue
+        except ValueError:
+                continue
+
+        if month < 10:
+            month = f"0{month}"
+        if day < 10:
+            day = f"0{day}"
+        print(f"{year}-{month}-{day}")
+        break
+
+main()
